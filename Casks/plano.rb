f0a9786e52341a64b783609d56ea5847eef061cc
plano-release <release@plano.sh>
2026-09-14 09:12:48 +0000

    Plano 0.1.11

--- Casks/plano.rb
+++ Casks/plano.rb
@@ -7,8 +7,8 @@
 # `brew upgrade` moves to the next release, since every release rewrites
 # this file with its own version and checksum.
 cask "plano" do
-  version "0.1.10"
-  sha256 "ea585548f87aba98238e69cbf3c9b806c72df5f07457a6420b61df3612e8e86d"
+  version "0.1.11"
+  sha256 "23432ca8a512f1fd035a0a3808a6cda98c693e2b4d3906a1b44c647bee075e69"
 
   url "https://github.com/plano-org/releases/releases/download/v#{version}/Plano-#{version}.dmg"
   name "Plano"
